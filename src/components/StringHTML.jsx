563c13799ff16f6f1f58cf493cd0a216cfb2c7a3
Paulo Ruan <110623212+PauloRuan30@users.noreply.github.com>
2024-02-19 13:15:18 -0300

    Delete src directory

--- src/components/StringHTML.jsx
+++
@@ -1,8 +0,0 @@
-export const StringHTML = ({ htmlString }) => {
-    //console.log(htmlString)
-    return (
-    <div class="text-2xl text-start mx-20 my-10">    
-        <div dangerouslySetInnerHTML={{__html: htmlString}} ></div>
-    </div>
-    )
-}
--- src/pages/inicio/Inicio.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Inicio = () => {
-    return (
-        <p>
-        </p>
-    );
-}
-export default Inicio;
